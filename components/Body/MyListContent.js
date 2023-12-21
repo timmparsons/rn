685a8cc5f41296d931dea3bf85e61387c3710312
Tim Parsons <timmparsons85@gmail.com>
2023-12-21 07:22:23 -0700

    updated database and worked on call t oo server from client

--- components/Body/MyListContent.js
+++ components/Body/MyListContent.js
@@ -1,15 +1,34 @@
 import { View, Text } from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { data } from '../../test/fixtures';
+import { getUsers } from '../../api';
 
 const MyListContent = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const userData = await getUsers();
+        {
+          userData ? setUsers(userData) : console.error('Invalid user data received:', userData);
+        }
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <View>
-      {data.map((movie) => {
-        return <Text key={movie.id}>{movie.title}</Text>;
-      })}
+      {users && users.length > 0 ? (
+        users.map((user) => <Text key={user.id}>{user.name}</Text>)
+      ) : (
+        <Text>No users available</Text>
+      )}
     </View>
   );
 };
-
 export default MyListContent;
